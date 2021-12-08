81d28e7243ed525ef0f08c314179b0ba39e9788a
bpkcongli <andrian8367@gmail.com>
2021-12-08 16:10:40 +0700

    Creating Authentications Service

---
+++ src/services/mongodb/AuthenticationsService.js
@@ -0,0 +1,26 @@
+const BadRequestError = require('../../exceptions/BadRequestError');
+const DatabaseUtils = require('../../utils/DatabaseUtils');
+
+class AuthenticationsService {
+  async addRefreshToken(refreshToken) {
+    const db = await DatabaseUtils.createConnection();
+    const authentications = await db.collection('authentications');
+    await authentications.insertOne({refresh_token: refreshToken});
+  }
+
+  async verifyRefreshToken(refreshToken) {
+    const db = await DatabaseUtils.createConnection();
+    const authentications = await db.collection('authentications');
+    const result = await authentications.findOne({refresh_token: refreshToken});
+
+    if (!result) throw new BadRequestError('Maaf, refresh token tidak dapat ditemukan di database.');
+  }
+
+  async deleteRefreshToken(refreshToken) {
+    const db = await DatabaseUtils.createConnection();
+    const authentications = await db.collection('authentications');
+    await authentications.deleteOne({refresh_token: refreshToken});
+  }
+}
+
+module.exports = AuthenticationsService;
